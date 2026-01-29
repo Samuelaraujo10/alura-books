c29b3c2480ac231227c063a89042c5b95589180e
Samuel Araújo <samuelaraujo71@gmail.com>
2026-01-29 15:15:15 -0300

    Add search functionality to Pesquisa component; create dadosPesquisa for book data

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'O Auto da Compadecida', id: 1, src:livro},
+    {nome: 'Dom Casmurro', id: 2, src:livro},
+    {nome: 'O Inferno de Dante', id: 3, src:livro},
+    {nome: 'Manifesto Comunista', id: 4, src:livro},
+    {nome: 'Harry Potter', id: 5, src:livro},
+]
+
--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 import Input from '../Input'
+import { useState } from 'react'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -22,12 +24,47 @@ const Subtitulo = styled.h3`
     margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
 function Pesquisa() {
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+
+    console.log(livrosPesquisados);
+    
+ 
     return (
         <PesquisaContainer>
-        <Titulo>Já sabe por onde começar?</Titulo>
-        <Subtitulo>Enconte seu livro em nossa estante.</Subtitulo>
-        <Input  placeholder="Escreva sua próxima leitura" />
+            <Titulo>Já sabe por onde começar?</Titulo>
+            <Subtitulo>Enconte seu livro em nossa estante.</Subtitulo>
+            <Input  placeholder="Escreva sua próxima leitura" 
+                    onBlur={evento => {
+                        const textoDigitado = evento.target.value
+                        const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado))
+                        setLivrosPesquisados(resultadoPesquisa)
+                    }}
+        />
+        { livrosPesquisados.map(livro => (
+            <Resultado>
+                <p>{livro.nome}</p>
+                <img src={livro.src}/>
+            </Resultado>
+             
+        ) ) }
         </PesquisaContainer>
     )
 }

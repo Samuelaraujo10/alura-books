f1a9a88e077ab28171524a7d7c4af7c5e5eb513a
Samuel Araújo <samuelaraujo71@gmail.com>
2026-04-10 15:51:24 -0300

    Add Favoritos component and implement favoritos data fetching

--- src/routes/Favoritos.js
+++ src/routes/Favoritos.js
@@ -1,17 +1,31 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Pesquisa from '../componentes/Pesquisa';
-const HomeContainer = styled.div`
-  
+import { getFavoritos } from '../services/favoritos';
+
+const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(90deg, #002F52 35%, #326589);
+  background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
 
 function Favoritos() {
+  const [favoritos, setFavoritos] = useState([])
+
+  async function fetchFavoritos() {
+    const favoritosDaAPI = await getFavoritos()
+    setFavoritos(favoritosDaAPI)
+  }
+
+  useEffect( () => {
+    fetchFavoritos()
+  }, [])
+
   return (
-    <HomeContainer>
-      <Pesquisa/>
-    </HomeContainer>
+    <AppContainer>
+      {favoritos.map( favorito => (
+        <p>{favorito.nome}</p>
+      ))}
+    </AppContainer>
   );
 }
 
